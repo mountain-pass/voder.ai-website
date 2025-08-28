261c88c40327e6e2d5dfb1699008fcf47c3ad88a
Tom Howard <tom.howard@greater.com.au>
2025-08-28 11:49:27 +1000

    chore: add jiti to peerDependencies for ESLint TS config loading and update vitest.config.ts to use createVitestNodeConfig

--- packages/dev-config/vitest.config.ts
+++ packages/dev-config/vitest.config.ts
@@ -1,16 +1,3 @@
-export default {
-  test: {
-    testTimeout: 120000,
-    environment: 'node',
-    globals: true,
-    setupFiles: ['./src/test-setup.node.ts'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html', 'lcov', 'json'],
-      include: ['src/**/*.ts'],
-      exclude: ['**/*.test.ts', '**/node_modules/**'],
-      thresholds: { branches: 90, functions: 90, lines: 90, statements: 90 },
-    },
-    retry: { runMode: 5 }
-  },
-} as const;
+import { createVitestNodeConfig } from '@voder/dev-config/testing';
+
+export default createVitestNodeConfig();
